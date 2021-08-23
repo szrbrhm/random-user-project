55f198d6a3c6367eae960eb7bb866ea7870d1c47
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-23 14:34:58 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
+import React,{useState, useEffect} from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
+import Card from './component/Card';
+
 
 function App() {
   const [itemList, setItemList] = useState([]);
 
   useEffect(() => {
-  //   fetch('https://https://randomuser.me/api/')
-  // .then(response => response.json())
-  // .then((data) => setItemList(data));
-  axios.get('https://randomuser.me/api/').then((res) => setItemList(res.data));
+    fetch('https://https://randomuser.me/api/results')
+  .then(response => response.json())
+  .then((data) => setItemList(data));
+  // axios.get('https://randomuser.me/api/results').then((res) => setItemList(res.data));
 
   },[])
   return (
     <div className="App">
-      <div className="container">
-        <div className="container-left">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        </div>
-        <div className="container-right">
-          <h2></h2>
-          <p></p>
-          <p></p>
-          <p></p>
-        </div>
-        <div>
-          <p></p>
-          <p></p>
-        </div>
-
-        
-      </div>
+    {itemList.map((item) => (
+      <Card key={item.id} item={item} />
+    ))}
+      
      
     </div>
   );
