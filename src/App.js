f15c0485ce364ba067f42f2fb446b85cfb05bcec
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-23 14:40:39 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import './App.css';
-// import axios from 'axios';
+import axios from 'axios';
 import Card from './component/Card';
 
 
@@ -8,10 +8,10 @@ function App() {
   const [itemList, setItemList] = useState([]);
 
   useEffect(() => {
-    fetch('https://https://randomuser.me/api/results')
-  .then(response => response.json())
-  .then((data) => setItemList(data));
-  // axios.get('https://randomuser.me/api/results').then((res) => setItemList(res.data));
+  //   fetch('https://https://randomuser.me/api/results')
+  // .then(response => response.json())
+  // .then((data) => setItemList(data));
+  axios.get('https://randomuser.me/api/').then((res) => setItemList(res.data));
 
   },[])
   return (
