c5acf1c3f79c0893bd714e39083201c458800141
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-24 16:55:23 +0200

    create card component

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import logoMail from '../assets/email.svg';
-import logoTelefon from '../assets/phone.svg';
-import logoLocation from '../assets/location.svg';
+import React from "react";
+import logoMail from "../assets/email.svg";
+import logoTelefon from "../assets/phone.svg";
+import logoLocation from "../assets/location.svg";
 
-const Card = ({item}) => {
-    return (
+const Card = ({ item }) => {
+  return (
     <div className="container">
-        <div className="container-left">
-        <img src={item.picture} alt="" />
-        <img src={logoMail} alt="" />
-        <img src={logoTelefon} alt="" />
-        <img src={logoLocation} alt="" />
-        </div>
-        <div className="container-right">
-          <h2>{item.name}</h2>
-          <p>{item.mail}</p>
-          <p>{item.phone}</p>
-          <p>{item.location}</p>
+      <div className="container">
+        <img src={item?.picture?.medium} alt="" />
+        <h3>{item?.name?.first} {item?.name?.last}</h3>
         </div>
         <div>
-          <p></p>
-          <p></p>
+          <img src={logoMail} alt="" />
+          <p>{item?.email}</p>
         </div>
-
-        
-      </div>
+        <div>
+          <img src={logoTelefon} alt="" />
+          <p>{item?.phone}</p>
+        </div>
+        <div>
+          <img src={logoLocation} alt="" />
+          <p>{item?.location?.city} {item?.location?.country}</p>
+        </div>
+          <p>Age: {item?.dob?.age}</p>
+        </div>
     
-    )
-}
+  );
+};
 
 export default Card;
