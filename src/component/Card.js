60db90c81886f4b5e6b7ef4edfd2ac340ddc81f8
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-24 23:45:04 +0200

    continue

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,23 +2,24 @@ import React from "react";
 import logoMail from "../assets/email.svg";
 import logoTelefon from "../assets/phone.svg";
 import logoLocation from "../assets/location.svg";
+import './Card.css';
 
 const Card = ({ item }) => {
   return (
     <div className="container">
-      <div className="container">
+      <div className="main">
         <img src={item?.picture?.medium} alt="" />
         <h3>{item?.name?.first} {item?.name?.last}</h3>
         </div>
-        <div>
+        <div className="main">
           <img src={logoMail} alt="" />
           <p>{item?.email}</p>
         </div>
-        <div>
+        <div className="main">
           <img src={logoTelefon} alt="" />
           <p>{item?.phone}</p>
         </div>
-        <div>
+        <div className="main">
           <img src={logoLocation} alt="" />
           <p>{item?.location?.city} {item?.location?.country}</p>
         </div>
