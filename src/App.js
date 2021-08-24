d151a170aab8e009e9df8eea9ee640fd119d85a5
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-08-24 16:55:03 +0200

    item component add

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import Card from './component/Card';
 
 
 function App() {
-  const [itemList, setItemList] = useState([]);
+  const [item, setItem] = useState();
+  const getItem = () => {
+    axios.get('https://randomuser.me/api/').then((res) => setItem(res?.data?.results[0]));
+
+  }
 
   useEffect(() => {
   //   fetch('https://https://randomuser.me/api/results')
   // .then(response => response.json())
   // .then((data) => setItemList(data));
-  axios.get('https://randomuser.me/api/').then((res) => setItemList(res.data));
-
+  getItem();
   },[])
   return (
     <div className="App">
-    {itemList.map((item) => (
-      <Card key={item.id} item={item} />
-    ))}
+    
+    
+      <Card item={item}/>
+    
+    <button onClick={getItem}>Random User</button>
       
      
     </div>
