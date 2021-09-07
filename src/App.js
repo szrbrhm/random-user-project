04385adb29c6c90676299b0b59b943287284c522
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-07 22:49:03 +0200

    finis project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
-import axios from 'axios';
-import Card from './component/Card';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Card from "./component/Card";
 
 function App() {
   const [item, setItem] = useState();
   const [changeUser, setChangeUser] = useState(0);
   const getItem = () => {
-    axios.get('https://randomuser.me/api/').then((res) => setItem(res?.data?.results[0]));
-    
-  }
+    axios
+      .get("https://randomuser.me/api/")
+      .then((res) => setItem(res?.data?.results[0]));
+  };
 
   useEffect(() => {
-  //   fetch('https://https://randomuser.me/api/results')
-  // .then(response => response.json())
-  // .then((data) => setItemList(data));
+    //   fetch('https://https://randomuser.me/api/results')
+    // .then(response => response.json())
+    // .then((data) => setItemList(data));
 
-  getItem();
-  // console.log(changeUser);
-  },[changeUser])
-  
+    getItem();
+    // console.log(changeUser);
+  }, [changeUser]);
+
   return (
     <div className="App">
-     
-     
-      <Card item={item}/>
+      <Card item={item} />
 
-    <button onClick={() => setChangeUser(changeUser + 1)}>Random User</button>
-      
-     
+      <button onClick={() => setChangeUser(changeUser + 1)}>Random User</button>
     </div>
   );
 }
