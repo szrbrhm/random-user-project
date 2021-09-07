2edd247e3fe09a92b02714145dcb7f3f40f4faf5
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-07 22:48:40 +0200

    add email,phone,location comp

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,68 +1,43 @@
 import React from "react";
-import logoMail from "../assets/email.svg";
-import logoTelefon from "../assets/phone.svg";
-import logoLocation from "../assets/location.svg";
+// import logoMail from "../assets/email.svg";
+// import logoTelefon from "../assets/phone.svg";
+// import logoLocation from "../assets/location.svg";
+import Email from "../svg/Email";
+import Phone from "../svg/Phone";
+import Location from "../svg/Location";
 import "./Card.css";
 
 const Card = ({ item }) => {
   return (
     <div className="container-main">
       <div className="main">
-        <img className="image" src={item?.picture?.medium} alt="" />
-
-        <h3>
-          {item?.name?.first} {item?.name?.last}
-        </h3>
+        <div className="image-title-box">
+          <img className="img" src={item?.picture?.medium} alt="" />
+          <p className="info text">
+            {item?.name?.first} {item?.name?.last}
+          </p>
+        </div>
+        <div className="user-email">
+          <Email className="img" />
+          <p className="info">{item?.email}</p>
+        </div>
+        <div className="user-phone">
+          <Phone className="img" />
+          <p className="info">{item?.phone}</p>
+        </div>
+        <div className="user-location">
+          <Location className="img" />
+          <p className="info">
+            {item?.location?.city} - {item?.location?.country}
+          </p>
+        </div>
+        <div>
+          <p>Age: {item?.dob?.age}</p>
+          <p>Register Date : {item?.registered?.date.split("T")[0]}</p>
+        </div>
       </div>
-      <div className="main">
-        <img src={logoMail} alt="" />
-        <p>{item?.email}</p>
-      </div>
-      <div className="main">
-        <img src={logoTelefon} alt="" />
-
-        <p>{item?.phone}</p>
-      </div>
-      <div className="main">
-        <img src={logoLocation} alt="" />
-
-        <p>
-          {item?.location?.city} - {item?.location?.country}
-        </p>
-      </div>
-      <p>Age: {item?.dob?.age}</p>
-      <p>Register Date : {item?.registered?.date.split("T")[0]}</p>
     </div>
   );
 };
 
 export default Card;
-
-// div className="main-left">
-//          <div>
-//         <img className="image" src={item?.picture?.medium} alt="" />
-//       </div>
-//       <div>
-//         <img src={logoMail} alt="" />
-//       </div>
-//       <div>
-//         <img src={logoTelefon} alt="" />
-//       </div>
-//       <div>
-//         <img src={logoLocation} alt="" />
-//       </div>
-//       </div>
-//       <div className="main-right">
-//       <div>
-//           <h3>{item?.name?.first} {item?.name?.last}</h3>
-//         </div>
-//         <div>
-//           <p>{item?.email}</p>
-//         </div>
-//         <div className="main">
-//           <p>{item?.phone}</p>
-//         </div>
-//         <div className="main">
-//           <p>{item?.location?.city} {item?.location?.country}</p>
-//         </div>
-//       </div>
